b23f212ee44f06853e0ccd4b25dc8199a6c04ff6
Luis0lo <58235960+Luis0lo@users.noreply.github.com>
2021-01-01 19:37:35 +0000

    several

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 //does active the nav section where we are
-$("body").scrollspy({ target: "#main-nav" });
+$('body').scrollspy({ target: '#main-nav' });
 
 //add smoth scrolling. slim cdn version doesnt work!
-$("#main-nav a").on("click", function (e) {
+$('#main-nav a').on('click', function (e) {
   // check for a hash value
-  if (this.hash !== "") {
+  if (this.hash !== '') {
     //prevent default behavior
     e.preventDefault();
     //store hash
     const hash = this.hash;
     //animate smooth scroll
-    $("html, body").animate(
+    $('html, body').animate(
       {
         scrollTop: $(hash).offset().top,
       },
@@ -23,4 +23,4 @@ $("#main-nav a").on("click", function (e) {
   }
 });
 // Get the current year for the copyright
-$("#year").text(new Date().getFullYear());
+$('#year').text(new Date().getFullYear());
